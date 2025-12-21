e751eeaf81c2861336d7e713be557502dda89a7e
hex4lcodes <hexasweetzmain@gmail.com>
2025-12-21 17:45:26 -0500

    Create hello-first.c
    I wrote a hello world program using printf! Im gonna try it using puts next.

---
+++ hello-first.c
@@ -0,0 +1,12 @@
+// this is my attempt to write hello world by hand 
+
+#include <stdio.h>
+
+int main() {
+
+  printf("Hello world");
+  return 0;
+
+}
+
+// this should run and say hello world....
