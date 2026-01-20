e822c45f9af4bde87fd6068f57f05ea9e1f6d089
Hex <hexasweetzmain@gmail.com>
2026-01-19 20:01:19 -0500

    Add conditions.c
    Eligibility checker using ternary operator

---
+++ begining-C/conditions.c
@@ -0,0 +1,18 @@
+//ELIGIBILITY 
+
+#include <stdio.h>
+
+int main() {
+    //age checker - NOT USING IF STATEMENTS
+
+    int age;
+
+    age = 12;
+
+    age > 18 ? printf("You are old enough to vote!") : printf("You cannot vote!"); //conditional ternary operator
+
+    //conditional statement ? if true : if false
+    
+
+   return 0; 
+}
